58f623f17eb297b17f79be6a4e928bb06612ee20
domcodes <email@domcodes.com>
2017-03-24 13:38:03 +0000

    Tidy up the code

--- scripts.js
+++ scripts.js
@@ -2,19 +2,12 @@
 	
 var visitor = prompt("What is your name?");
 	
-	
 //2. Combine strings so this prints to page. Hello [  ]. Welcome to Treehouse.We are so glad that you came by to visit, [   ]. Please come again, when you want to learn some more.
 
-
 var message = "Hello " + visitor + ".  Welcome to Treehouse.";
-
-message += " We are so glad that you came by to visit, ";
-
-message += visitor;
-
-message += ". Please come again, when you want to learn some more."
-
-
+		message += " We are so glad that you came by to visit, ";
+		message += visitor;
+		message += ". Please come again, when you want to learn some more."
 
 //3. write it to the page
 
