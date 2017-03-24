31ca744192f443f516418bc3ea4df8081b87ee16
domcodes <email@domcodes.com>
2017-03-24 13:19:53 +0000

    Capture user input, start combining strings

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
 //1. Capture vistor input
 	
+var visitor = prompt("What is your name?");
 	
 	
-	
-	
-//2. Combine strings
-	
+//2. Combine strings so this prints to page. Hello [  ]. Welcome to Treehouse.We are so glad that you came by to visit, [   ]. Please come again, when you want to learn some more.
+
+
+var message = "Hello " + visitor + ".  Welcome to Treehouse.";
+
+
+
 
 
 
 
 
 //3. write it to the page
+
+document.write(message);
